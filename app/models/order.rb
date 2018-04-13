44cb65d68e9ef8b94bfd7ddaed7cd66b3602acd8
Vishal Yadav <vishalyuvasoft126@gmail.com>
2018-04-13 09:47:16 +0530

    total calculations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
 	has_many :order_items, :dependent => :destroy
   accepts_nested_attributes_for :order_items, reject_if: :all_blank, allow_destroy: true
+
+  def cost_total
+  	self.order_items.pluck(:cost).sum()
+  end
+
+  def sale_tax_total
+  	self.order_items.pluck(:sale_tax).sum()
+  end
 end
